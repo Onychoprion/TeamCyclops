a99cd1a5e4e6594d1f5170dbebaab101ed8156ca
John Zeller <johnlzeller@gmail.com>
2013-11-20 15:52:15 -0800

    Changed full template over to passpro

--- passpro/chrome/content/linkTargetFinder.js
+++ passpro/chrome/content/linkTargetFinder.js
@@ -1,34 +1,34 @@
-var linkTargetFinder = function () {
+var passpro = function () {
 	var prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
 	return {
 		init : function () {
 			gBrowser.addEventListener("load", function () {
-				var autoRun = prefManager.getBoolPref("extensions.linktargetfinder.autorun");
+				var autoRun = prefManager.getBoolPref("extensions.passpro.autorun");
 				if (autoRun) {
-					linkTargetFinder.run();
+					passpro.run();
 				}
 			}, false);
 		},
 			
 		run : function () {
 			var head = content.document.getElementsByTagName("head")[0],
-				style = content.document.getElementById("link-target-finder-style"),
+				style = content.document.getElementById("pass-pro-style"),
 				allLinks = content.document.getElementsByTagName("a"),
 				foundLinks = 0;
 			
 			if (!style) {
 				style = content.document.createElement("link");
-				style.id = "link-target-finder-style";
+				style.id = "pass-pro-style";
 				style.type = "text/css";
 				style.rel = "stylesheet";
-				style.href = "chrome://linktargetfinder/skin/skin.css";
+				style.href = "chrome://passpro/skin/skin.css";
 				head.appendChild(style);
 			}	
 						
 			for (var i=0, il=allLinks.length; i<il; i++) {
 				elm = allLinks[i];
 				if (elm.getAttribute("target")) {
-					elm.className += ((elm.className.length > 0)? " " : "") + "link-target-finder-selected";
+					elm.className += ((elm.className.length > 0)? " " : "") + "pass-pro-selected";
 					foundLinks++;
 				}
 			}
@@ -41,4 +41,4 @@ var linkTargetFinder = function () {
 		}
 	};
 }();
-window.addEventListener("load", linkTargetFinder.init, false);
+window.addEventListener("load", passpro.init, false);
